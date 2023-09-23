903416e43c9d01c1732f7e5890836910a30c1adc
Lera <128050534+MsMiracle18@users.noreply.github.com>
2023-09-23 18:45:49 +0200

    Update models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.name
 
+class Quote(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.text
