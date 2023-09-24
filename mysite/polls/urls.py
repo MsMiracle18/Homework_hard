461a6000453ad022a3ef5b9a75dc2b371dec1f33
Lera <128050534+MsMiracle18@users.noreply.github.com>
2023-09-24 08:34:28 +0200

    Update urls.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('add_author/', views.add_author, name='add_author'),
-    path('add_quote/', views.add_quote, name='add_quote'),
+    # URL-шляхи для вашого додатку
+    path('', views.index, name='index'),
+    path('authors/', views.author_list, name='author_list'),
+    path('authors/<int:author_id>/', views.author_detail, name='author_detail'),
+    path('quotes/', views.quote_list, name='quote_list'),
+    path('quotes/<int:quote_id>/', views.quote_detail, name='quote_detail'),
+    path('tags/', views.tag_list, name='tag_list'),
+    path('tags/<int:tag_id>/', views.tag_detail, name='tag_detail'),
 ]
